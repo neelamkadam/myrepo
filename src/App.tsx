754a8ad4b22aa23c3e6f6f1a029a1c2a4e89bd54
neelamkadam <kadamneelam7070@gmail.com>
2025-04-19 17:51:39 +0530

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Button } from './components/ui/button'
 import './App.css'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Button variant="destructive" className='text-rose-400' size={"sm"}>Click me</Button>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
